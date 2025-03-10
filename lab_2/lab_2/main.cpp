21f2e102366bf117bafb18fb997dbf21a0d31da3
sveta-terentyeva <svetik.terentyeva@gmail.com>
2025-03-10 21:16:38 +0200

    Create main.cpp

---
+++ lab_2/lab_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab_2
+//
+//  Created by Света on 10.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
